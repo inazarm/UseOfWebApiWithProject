9abe629460bdfea5098fe9543e38ebd9ad5bb750
Nazar.M <inazar.m@gmail.com>
2020-08-12 23:57:19 +0500

    Controller Updated
    Controller updated with Actions, Index and Create

--- UseOfWebApiWithProject/Controllers/HomeController.cs
+++ UseOfWebApiWithProject/Controllers/HomeController.cs
@@ -12,10 +12,33 @@ namespace UseOfWebApiWithProject.Controllers
 {
     public class HomeController : Controller
     {
+        IEnumerable<Employee> students = null;
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/Api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Employees");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Employee>>();
+                    readTask.Wait();
+
+                    students = readTask.Result;
+                }
+                else
+                {
+                    students = Enumerable.Empty<Employee>();
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(students);
+
         }
 
         public ActionResult Create()
@@ -26,7 +49,21 @@ namespace UseOfWebApiWithProject.Controllers
         [HttpPost]
         public ActionResult Create(Employee emp)
         {
-            return View();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/Api/Home");
+                var postTask = client.PostAsJsonAsync<Employee>("Employees", emp);
+                postTask.Wait();
+                var Result = postTask.Result;
+                if (Result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "some error");
+            return View(emp);
         }
+
     }
 }
