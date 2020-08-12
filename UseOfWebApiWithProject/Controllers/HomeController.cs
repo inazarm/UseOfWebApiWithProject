2ee5257a014e7e29b377f205db6dbb4e8ccef81e
Nazar.M <inazar.m@gmail.com>
2020-08-12 18:30:44 +0500

    Initial commit

---
+++ UseOfWebApiWithProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.CompensatingResourceManager;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using UseOfWebApiWithProject.Models;
+
+namespace UseOfWebApiWithProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            return View();
+        }
+    }
+}
